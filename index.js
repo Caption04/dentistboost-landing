9a1db8549e9e93407e53beda7b2d32a81a30ef36
Caption04 <kgkatunga04@gmail.com>
2025-09-19 09:42:08 +0200

    Green

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const openMenu = document.querySelector(".open-menu");
-const sideBar = document.querySelector(".sidebar")
+const sideBar = document.querySelector(".sidebar");
+const overlay = document.querySelector(".overlay");
 openMenu.addEventListener("click", () => {
     sideBar.classList.add("open");
+    overlay.classList.add("open");
 })

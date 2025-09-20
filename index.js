09b9852d1501b4bd050ea49134c68678896b28c7
Caption04 <kgkatunga04@gmail.com>
2025-09-20 08:43:20 +0200

    green

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const overlay = document.querySelector(".overlay");
 openMenu.addEventListener("click", () => {
     sideBar.classList.add("open");
     overlay.classList.add("open");
+})
 
-    openMenu.style.background = "red";
+const closemenu = document.querySelector(".close-menu");
+closemenu.addEventListener("click", () => {
+    sideBar.classList.remove("open");
+    overlay.classList.remove("open");
 })
